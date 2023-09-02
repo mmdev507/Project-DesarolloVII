842e4fa0353096e9d3a010e5e0e284ea4fe7826c
mmdev507 <pruebapd123@gmail.com>
2023-09-02 16:16:14 -0500

    realizado el laboratorio 1.3

---
+++ lab1/lab13.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+ <title> Laboratorio 1.3</title>
+</head>
+<body>
+<center>   
+<?php
+//Laboratorio 1.3: hacer un programa que muestre en pantalla información de PHP con la función
+//phpinfo(). Muestre la información centrada horizontalmente en la pantalla
+ echo phpinfo();
+?>
+</center>
+</body>
+</html>
