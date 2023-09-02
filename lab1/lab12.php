34b8901cdd5bfa25c41c5d8d0c4ee04c8f2f8498
mmdev507 <pruebapd123@gmail.com>
2023-09-02 15:58:16 -0500

    laboratorio 1.2 realizado

---
+++ lab1/lab12.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+ <title> </title>
+</head>
+<body>
+<?php
+//Laboratorio 1.2: Hacer un programa que sume dos variables que almacenan dos números distintos.
+ $n1 = 1;
+ $n2 = 2;
+ $suma = $n1 + $n2;
+ echo "suma = " .$suma. "<br>";
+ echo "$n1+$n2";
+?>
+</body>
+</html>
