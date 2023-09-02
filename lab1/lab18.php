55021f78bd8a48541bf2346598a46f9ce8564584
mmdev507 <pruebapd123@gmail.com>
2023-09-02 18:43:51 -0500

    Laboratorios 1.7 y 1.8 realizados

---
+++ lab1/lab18.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+ </head>
+<body>
+  <?php
+  //Manejo de variables II
+  $a = 8;
+  $b = 3;
+  echo $a + $b, "<br>";
+  echo $a - $b, "<br>";
+  echo $a * $b, "<br>";
+  echo $a / $b, "<br>";
+  $a++; echo $a, "<br>";
+  $b--; echo $b, "<br>";
+  ?>
+</body>
+</html>
+
