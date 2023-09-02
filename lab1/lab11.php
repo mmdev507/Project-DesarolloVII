de098ecf797feff7abd57cf4a7ec1b5e098789d8
mmdev507 <pruebapd123@gmail.com>
2023-09-02 15:41:04 -0500

    primer programa

---
+++ lab1/lab11.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+ <title> </title>
+</head>
+<body>
+<?php
+//Laboratorio 1.1: Concatenar dos cadenas con el operador punto (.) e imprimir su resultado.
+ $ini = " Hola ";
+ $fin = " a todos ";
+ $todo = $ini.$fin;
+ echo $todo;
+?>
+</body>
+</html>
