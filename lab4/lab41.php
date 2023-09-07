7369adfeeedf2680dc412dabd366a780ecc77b9f
mmdev507 <HD@gpc-helpdesk.sertracen.net>
2023-09-06 20:43:38 -0500

    Laboratorio 4.1 Aplicación web con imagen dinámica

---
+++ lab4/lab41.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+ <title> Laboratorio 4.1</title>
+</head>
+<body>  
+<?php
+
+if (array_key_exists('enviar', $_POST)){ 
+    if ($_REQUEST['Valorventa'] >= 80)
+    { 
+        echo '<td><img src="/desarollo%20VII/Project-DesarolloVII/lab4/verde.jpg"><br/>';
+        echo "El resultado de venta digitado fue: $_REQUEST[Valorventa] %";
+    }
+   else if ($_REQUEST['Valorventa'] >=50 and $_REQUEST['Valorventa'] <80)
+    { 
+        echo '<td><img src="/desarollo%20VII/Project-DesarolloVII/lab4/amarillo.jpg"><br/>';
+        echo "El resultado de venta digitado fue: $_REQUEST[Valorventa] %";
+    }
+     else
+     { 
+        echo '<td><img src="/desarollo%20VII/Project-DesarolloVII/lab4/rojo.jpg"><br/>';
+        echo "El resultado de venta digitado fue: $_REQUEST[Valorventa] %" ;
+     }
+    echo "<BR><BR>";
+}
+?>
+<button onclick="history.back()">Regresar</button>
+</body>
+</html>
